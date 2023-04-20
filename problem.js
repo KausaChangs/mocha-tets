ab959139708c1e8487a72ab56b49075d8bdd692b
KausaChangs <changayakausa@gmail.com>
2023-04-20 10:11:31 +0200

    created a describe for test.js

--- problem.js
+++ problem.js
@@ -1,6 +1,7 @@
 //farenheit to celcius
+//[23, 140, 212, 41] => [-5, 60,100, 5]
+// const farenheit = [23, 140, 212, 41];
 
-const farenheit = [23, 140, 212, 41];
-function convertToCelcius() {
+function convertToCelcius(farenheit) {
   return farenheit.map((value) => (value - 32) * 5.9);
 }
--- tests.js
+++ tests.js
@@ -1,1 +1,10 @@
+const chai = window.chai;
+const expect = chai.expect;
 
+describe("convertToCelcius", () => {
+  it("Should convert to farenheit to celcius for all values in an array", () => {
+    expect(convertToCelcius([23, 140, 212, 41])).to.deep.equal([
+      -5, 60, 100, 5,
+    ]);
+  });
+});
