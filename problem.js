f186ed7d4303ac9d8b2af4de36c4f0a0561a47cf
KausaChangs <changayakausa@gmail.com>
2023-04-20 15:43:18 +0200

    test isnt running

--- problem.js
+++ problem.js
@@ -2,6 +2,8 @@
 //[23, 140, 212, 41] => [-5, 60,100, 5]
 // const farenheit = [23, 140, 212, 41];
 
-function convertToCelcius(farenheit) {
-  return farenheit.map((value) => (value - 32) * 5.9);
+function getCelcius(fahrenheit) {
+  return fahrenheit.map((value) => {
+    (value - 32) * (5 / 9);
+  });
 }
--- tests.js
+++ tests.js
@@ -1,10 +1,8 @@
 const chai = window.chai;
 const expect = chai.expect;
 
-describe("convertToCelcius", () => {
-  it("Should convert to farenheit to celcius for all values in an array", () => {
-    expect(convertToCelcius([23, 140, 212, 41])).to.deep.equal([
-      -5, 60, 100, 5,
-    ]);
+describe("getCelcius", () => {
+  it("Should convert farenheit to celcius for all values in an array", () => {
+    expect(getCelcius([23, 140, 212, 41])).to.deep.equal([-5, 60, 100, 5]);
   });
 });
